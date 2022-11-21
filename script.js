6c0c8293c3fde203819a1cb5d4d1a89e865e686d
Laos22 <112620292+Laos22@users.noreply.github.com>
2022-11-21 16:54:04 +0200

    Звук и начал поле старт

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // -------------------------------------------ПЕРЕМЕННЫЕ
 let board = document.querySelector(".board");
 let cursor = document.querySelector(".cursor");
+let info = document.querySelector(".info");
+let startBoard = document.querySelector(".start");
+let body = document.querySelector("body");
+let title = document.querySelector("title");
+let btnStart = document.querySelector("#btnStart");
+let btnRestart = document.querySelector(".btnRestart");
+let btnRestart2 = document.querySelector(".btnRestart2");
+let game_block = document.querySelector(".game_block");
+let endGame_block = document.querySelector(".endGame");
+let textLoose = document.querySelector(".textLoose");
+let gameWin = document.querySelector(".gameWin");
+let textWin = document.querySelector(".textWin");
+
+
 let holesList = [];
-let gridBoard = 4;
+let gridBoard = 3;
 let countHole = gridBoard * gridBoard;
-let sizeHole = board.offsetHeight / gridBoard * 0.85;
+let sizeHole = 420 / gridBoard * 0.85;
 let frequencySpike = 2;
 let updateAnim = 100;
 let updateMove = 50;
@@ -13,17 +27,15 @@ let scoreBoard = document.querySelector(".score");
 let countLifes = 3;
 
 //---------------- Выполнение программы
-
-
-fillBoard(countHole);
-startTimer(10);
-createLifes();
-addEnemy();
-
-
-
-
-
+btnStart.onclick = function() {
+    startGame();
+}
+btnRestart.onclick = function() {
+    location.reload();
+}
+btnRestart2.onclick = function() {
+    location.reload();
+}
 
 
 
@@ -36,7 +48,37 @@ addEnemy();
 // -------------------------------------------- ФУНКЦИИ -------------------------------------------
 // ------------------------------------------------------------------------------------------------
 
-// ==================================================================== Проверка на попадание
+
+
+function startGame() {
+    startBoard.style.display = "none";
+    game_block.style.display = "flex";
+    fillBoard(countHole);
+    startTimer(5);
+    createLifes();
+    addEnemy();
+    cheekMouse();
+    cursor.style.backgroundImage = "url('img/cursor3.png')";
+    body.style.cursor = "none";
+}
+
+function endGame() {
+    game_block.style.display = "none";
+    endGame_block.style.display = "flex";
+    cursor.style.backgroundImage = "none";
+    body.style.cursor = "auto";
+    textLoose.innerHTML = "Вы проиграли!!! Ваш счет: " + score;
+}
+
+function winGame() {
+    game_block.style.display = "none";
+    gameWin.style.display = "flex";
+    cursor.style.backgroundImage = "none";
+    body.style.cursor = "auto";
+    textWin.innerHTML = "Вы выиграли!!! Ваш счет: " + score;
+}
+
+
 
 
 
@@ -71,11 +113,7 @@ function addSpike() {
     let x = random(0, countHole - 1);
     holesList[x].appendChild(spike);
     animSpike(spike);
-    // holesList[x].id = "act";
-    // console.log(holesList[x].id);
     spike.className = "act";
-
-    // console.dir(spike)
 }
 //========================================================================== Анимация движение Спайки
 //----- Анимация спайка лай
@@ -208,10 +246,9 @@ function startTimer (n) {
     setInterval(function() {
         timer.innerHTML = count + "";
         count--;
-        if (count == -1) {
-            // alert("Таймер окончен, хотите перезапустить страницу?");
+        if (count == -2) {
             count = n;
-            // location.reload;
+            winGame()
         }
     }, 1000);
  }
@@ -237,41 +274,70 @@ function createLifes() {
 function lifesDown() {
     countLifes = countLifes - 1;
     if (countLifes <= 0) {
-        // endGame();
-        alert("Закончились жизни перезагрузка страницы");
-        location.reload();
+        endGame();
     }
     createLifes();
 }
 
 //============================================================================== Курсор
+
 window.addEventListener('mousemove', e => {
     cursor.style.top = e.pageY + 'px'
     cursor.style.left = e.pageX + 'px'
 
 })
-window.addEventListener('mousedown', () => {
-    cursor.classList.add('active')
-    let enemytemp = document.querySelector(".act");
-    enemytemp.addEventListener('click', () => {
+
+let cheekMouse = function() {
+    window.addEventListener('mousedown', () => {
+        cursor.classList.add('active')
+        let enemytemp = document.querySelector(".act");
+        enemytemp.addEventListener('click', () => {
             if (enemytemp.id == "jerry") {
+                soundClickMouse();
                 score ++;
                 scoreBoard.textContent = "Рахунок: " + score; // переопределить значение очков"
                 enemytemp.className = "";
                 enemytemp.style.display = "none";
-            } else {
+            } else if (enemytemp.id == "spike") {
+                soundClickDog();
                 lifesDown();
                 enemytemp.className = "";
                 enemytemp.style.display = "none";
             }
+        })
+        soundClickEmpty()
+        
     })
-    
+}
 
-   
-})
+
 window.addEventListener('mouseup', () => {
     cursor.classList.remove('active')
 })
+
+
+
+//============================================================================== Звуковое сопровождение
+//музыка
+function soundClickDog() { // функция для проигрывания звука удара, используется при клике во время игры
+    var audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = 'sound/dog.mp3'; // Указываем путь к звуку "клика"
+    audio.volume = 0.7;
+    audio.autoplay = true; // Автоматически запускаем
+}
+function soundClickMouse() { // функция для проигрывания звука удара, используется при клике во время игры
+    var audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = 'sound/target.mp3'; // Указываем путь к звуку "клика"
+    audio.volume = 0.7;
+    audio.autoplay = true; // Автоматически запускаем
+}
+function soundClickEmpty() { // функция для проигрывания звука удара, используется при клике во время игры
+    var audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = 'sound/empty.mp3'; // Указываем путь к звуку "клика"
+    audio.volume = 0.7;
+    audio.autoplay = true; // Автоматически запускаем
+}
+
 
 // ============================================================================ Функция рандом от и до включительно
 function random(min, max) {
